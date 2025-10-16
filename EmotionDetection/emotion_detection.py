ad66250aed6ea2198f7306b2d9ccfdb369ee1b46
Maddi Nav Preethivardhan <78253070+NavPreethivardhan@users.noreply.github.com>
2025-10-16 18:02:52 +0530

    Fix formatting issues in emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,7 @@ def emotion_detector(text_to_analyse):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' 
     jsonobj = { "raw_document": { "text": text_to_analyse } } 
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
+    #responds
     response = requests.post(URL, json = jsonobj, headers=header) 
     formatted_response = json.loads(response.text)
 
